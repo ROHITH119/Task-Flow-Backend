e04c40c2eb80b176f63b28121c0bc3ee355be059
Rohith Kodali <kodalirohith119@gmail.com>
2026-02-11 17:10:55 +0530

    added cors enforcement

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ const errorMiddleware = require("./middlewares/error.middleware")
 const logger = require("./middlewares/logger.middleware")
 const userRoutes = require("./routes/user.routes")
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://taskflowlite.netlify.app"
+    ]
+}))
 app.use(express.json())
 
 app.use(logger)
