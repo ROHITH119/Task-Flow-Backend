ba8a39bf1ed964909843f275706705e3ab10ab91
Rohith Kodali <kodalirohith119@gmail.com>
2026-02-16 15:23:23 +0530

    checking nodemailer prod

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -2,7 +2,11 @@ const nodemailer = require("nodemailer")
 
 const sendEmail = async({to, subject, html}) => {
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        service: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        family: 4,
+
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
