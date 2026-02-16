4ae562a3f3c452414ea48fea57e5147c1651290b
Rohith Kodali <kodalirohith119@gmail.com>
2026-02-16 15:30:56 +0530

    checking nodemailer prod

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,26 +1,25 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
-const sendEmail = async({to, subject, html}) => {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        family: 4,
+const sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // TLS
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        }
-    })
+  await transporter.sendMail({
+    from: `TaskFlow <${process.env.EMAIL_USER}>`,
+    to: to,
+    subject: subject,
+    html: html,
+  });
+};
 
-    await transporter.sendMail({
-        from: `TaskFlow <${process.env.EMAIL_USER}>`,
-        to: to,
-        subject: subject,
-        html: html
-    })
-}
-
-module.exports = {sendEmail}
-
-
+module.exports = { sendEmail };
